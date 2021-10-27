f45303510e2d4a4ee30e7be14c557b0f66ac6fb3
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-10-27 20:30:41 +0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  
+  return (
+    <SafeAreaView >
+     <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
